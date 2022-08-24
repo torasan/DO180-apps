bb3a7040506e064840759416fdb80326ca4d22d1
Tora San <battambang@gmail.com>
2022-08-24 21:05:37 +1000

    updated app

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "This application was created using console-openshift-console!\n";
+print "Hello, World! php version is " . PHP_VERSION . "<br />";
+print "A change is in the air!";
 ?>
